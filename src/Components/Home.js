9a576af32cc695636ba7e1d5b6d102ad3f193c2a
Joe Del Balzo <jdelbalzo99@gmail.com>
2023-08-22 17:32:30 -0400

    lightweight updates

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,18 +5,17 @@ import Contact from "./Contact";
 const Home = () => {
   return (
     <FadeComponent>
-      <div className="home-intro-paragraph" style={{ marginBottom: "5rem" }}>
+      <div className="home-intro-paragraph" style={{ marginBottom: "4rem" }}>
         <h1 className="home-intro-paragraph" style={{ display: "inline" }}>
           Hi, I’m Dawn, a professional proofreader and copyeditor with fifteen years of publishing
           experience
         </h1>
         , from Middle Grade and YA novels, to Graphic Novels, and more.
         <br />
-        <div style={{ margin: "auto", textAlign: "center", paddingTop: "1rem" }}>
+        <div style={{ margin: "auto", textAlign: "center", paddingTop: "2rem" }}>
           Let's make your words shine!
         </div>
       </div>
-
       <Contact />
     </FadeComponent>
   );
